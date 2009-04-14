42da1d2aeb955eebba382f1a142e15d7273021c8
Emmanuele Bassi <ebassi@linux.intel.com>
2009-04-14 17:16:41 +0100

    [build] Improve build environment
    * De-crustify the configure.ac file by using AS_IF(), AS_CASE()
      and other useful macros
    
    * Update all the variables
    
    * Fix the pkg-config file Requires:, and expose the flavour
      and API version as variables
    
    * Fix the autogen.sh script
    
    * Use Shave to clean up the libtool and gcc incantations and
      make the output useful again

--- autogen.sh
+++ autogen.sh
@@ -1,4 +1,28 @@
 #! /bin/sh
-gtkdocize || exit 1
-autoreconf -v --install || exit 1
-./configure --enable-maintainer-mode "$@"
+
+PKG_NAME=Clutter-Gtk
+TEST_TYPE=-d
+FILE=clutter-gtk
+
+srcdir=`dirname $0`
+test -z "$srcdir" && srcdir=.
+
+test $TEST_TYPE $FILE || {
+        echo "You must run this script in the top-level $PROJECT directory"
+        exit 1
+}
+
+gtkdocize || exit $?
+
+# we need to patch gtk-doc.make to support pretty output with
+# libtool 1.x.  Should be fixed in the next version of gtk-doc.
+# To be more resilient with the various versions of gtk-doc one
+# can find, just sed gkt-doc.make rather than patch it.
+sed -e 's#) --mode=compile#) --tag=CC --mode=compile#' gtk-doc.make > gtk-doc.temp \
+        && mv gtk-doc.temp gtk-doc.make
+sed -e 's#) --mode=link#) --tag=CC --mode=link#' gtk-doc.make > gtk-doc.temp \
+        && mv gtk-doc.temp gtk-doc.make
+
+ACLOCAL="${ACLOCAL-aclocal} $ACLOCAL_FLAGS" autoreconf -v --install || exit $?
+
+./configure "$@" && echo Now type make to compile $PKG_NAME.
