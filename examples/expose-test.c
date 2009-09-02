54ab5c6079ad6b1364eb0d9897a9f229601ad948
Danielle Madeley <danielle.madeley@collabora.co.uk>
2009-09-02 15:40:47 +1000

    A lot more feedback in expose-test

--- examples/expose-test.c
+++ examples/expose-test.c
@@ -26,6 +26,39 @@ expose_monitor (GtkWidget *widget, GdkEventExpose *event, char *str)
     return FALSE;
 }
 
+static gboolean
+damage_monitor (GtkWidget *widget, GdkEventExpose *event, char *str)
+{
+    g_print ("damage (%s) - (%i, %i, %i, %i)\n",
+            str,
+            event->area.x, event->area.y,
+            event->area.width, event->area.height);
+
+    return FALSE;
+}
+
+static gboolean
+press_monitor (GtkWidget *widget, GdkEventExpose *event, char *str)
+{
+    g_print ("press (%s)\n", str);
+
+    return FALSE;
+}
+
+static gboolean
+release_monitor (GtkWidget *widget, GdkEventExpose *event, char *str)
+{
+    g_print ("release (%s)\n", str);
+
+    return FALSE;
+}
+
+static void
+paint_monitor (ClutterActor *actor, char *str)
+{
+    g_print ("paint (%s)\n", str);
+}
+
 static GtkWidget *
 add_button (GtkTable *table, char *stock_id, int row)
 {
@@ -38,6 +71,10 @@ add_button (GtkTable *table, char *stock_id, int row)
 
     g_signal_connect (button, "expose-event",
             G_CALLBACK (expose_monitor), stock_id);
+    g_signal_connect (button, "button-press-event",
+            G_CALLBACK (press_monitor), stock_id);
+    g_signal_connect (button, "button-release-event",
+            G_CALLBACK (release_monitor), stock_id);
 
     return button;
 }
@@ -61,6 +98,19 @@ main (int argc, char **argv)
             G_CALLBACK (expose_monitor), "window");
     g_signal_connect (table, "expose-event",
             G_CALLBACK (expose_monitor), "table");
+
+    ClutterActor *stage = gtk_clutter_window_get_stage (GTK_CLUTTER_WINDOW (window));
+    ClutterActor *actor = clutter_group_get_nth_child (CLUTTER_GROUP (stage), 0);
+    GtkWidget *offscreen = gtk_clutter_actor_get_widget (GTK_CLUTTER_ACTOR (actor));
+
+    g_signal_connect (stage, "paint",
+            G_CALLBACK (paint_monitor), "stage");
+    g_signal_connect (actor, "paint",
+            G_CALLBACK (paint_monitor), "actor");
+    g_signal_connect (offscreen, "expose-event",
+            G_CALLBACK (expose_monitor), "offscreen");
+    g_signal_connect (offscreen, "damage-event",
+            G_CALLBACK (damage_monitor), "offscreen");
 
     gtk_container_add (GTK_CONTAINER (window), table);
     gtk_widget_show_all (window);
