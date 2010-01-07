54f22615a47e6e1993fa3fb9e89c299b4a947291
Emmanuele Bassi <ebassi@linux.intel.com>
2010-01-07 11:01:10 +0000

    window: Mark internal children as such
    The GtkClutterEmbed inside a GtkClutterWindow is private and should be
    marked as such.

--- clutter-gtk/gtk-clutter-window.c
+++ clutter-gtk/gtk-clutter-window.c
@@ -243,11 +243,17 @@ gtk_clutter_window_init (GtkClutterWindow *self)
 
   self->priv = priv = GTK_CLUTTER_WINDOW_GET_PRIVATE (self);
 
+  gtk_widget_push_composite_child ();
+
   priv->embed = gtk_clutter_embed_new ();
-  stage = gtk_clutter_embed_get_stage (GTK_CLUTTER_EMBED (priv->embed));
+  gtk_widget_set_composite_name (priv->embed, "GtkClutterEmbed");
 
   GTK_CONTAINER_CLASS (gtk_clutter_window_parent_class)->add (GTK_CONTAINER (self), priv->embed);
   gtk_widget_show (priv->embed);
+
+  gtk_widget_pop_composite_child ();
+
+  stage = gtk_clutter_embed_get_stage (GTK_CLUTTER_EMBED (priv->embed));
 
   priv->actor = gtk_clutter_actor_new ();
   clutter_container_add_actor (CLUTTER_CONTAINER (stage), priv->actor);
