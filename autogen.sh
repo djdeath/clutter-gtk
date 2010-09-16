40aae682b1823f04e5407469d7e923621541a074
Emmanuele Bassi <ebassi@linux.intel.com>
2010-09-16 14:38:22 +0100

    build: Update to something resembling sanity

--- autogen.sh
+++ autogen.sh
@@ -1,8 +1,8 @@
 #! /bin/sh
 
 PKG_NAME=Clutter-Gtk
-TEST_TYPE=-d
-FILE=clutter-gtk
+TEST_TYPE=-f
+FILE=clutter-gtk/clutter-gtk.h
 
 srcdir=`dirname $0`
 test -z "$srcdir" && srcdir=.
@@ -13,15 +13,6 @@ test $TEST_TYPE $FILE || {
 }
 
 gtkdocize || exit $?
-
-# we need to patch gtk-doc.make to support pretty output with
-# libtool 1.x.  Should be fixed in the next version of gtk-doc.
-# To be more resilient with the various versions of gtk-doc one
-# can find, just sed gkt-doc.make rather than patch it.
-sed -e 's#) --mode=compile#) --tag=CC --mode=compile#' gtk-doc.make > gtk-doc.temp \
-        && mv gtk-doc.temp gtk-doc.make
-sed -e 's#) --mode=link#) --tag=CC --mode=link#' gtk-doc.make > gtk-doc.temp \
-        && mv gtk-doc.temp gtk-doc.make
 
 ACLOCAL="${ACLOCAL-aclocal} $ACLOCAL_FLAGS" autoreconf -v --install || exit $?
 
