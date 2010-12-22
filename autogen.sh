c35a80b9de69c15fabe6171bcd17ae00c28434d2
Emmanuele Bassi <ebassi@linux.intel.com>
2010-12-22 16:33:38 +0000

    build: Update autogen.sh

--- autogen.sh
+++ autogen.sh
@@ -1,28 +1,55 @@
 #! /bin/sh
 
-PKG_NAME=Clutter-Gtk
-TEST_TYPE=-d
-FILE=clutter-gtk
-
-srcdir=`dirname $0`
+rcdir=`dirname $0`
 test -z "$srcdir" && srcdir=.
+
+ORIGDIR=`pwd`
+
+cd $srcdir
+PROJECT=Clutter-GTK
+TEST_TYPE=-f
+FILE=clutter-gtk/clutter-gtk.h
 
 test $TEST_TYPE $FILE || {
         echo "You must run this script in the top-level $PROJECT directory"
         exit 1
 }
 
+if automake-1.11 --version < /dev/null > /dev/null 2>&1 ; then
+    AUTOMAKE=automake-1.11
+    ACLOCAL=aclocal-1.11
+else
+        echo
+        echo "You must have automake 1.11.x installed to compile $PROJECT
+ECT."
+        echo "Install the appropriate package for your distribution,"
+        echo "or get the source tarball at http://ftp.gnu.org/gnu/automake/"
+        exit 1
+fi
+
+(gtkdocize --version) < /dev/null > /dev/null 2>&1 || {
+        echo
+        echo "You must have gtk-doc installed to compile $PROJECT."
+        echo "Install the appropriate package for your distribution,"
+        echo "or get the source tarball at http://ftp.gnome.org/pub/GNOME/sources/gtk-doc/"
+        exit 1
+}
+
+# NOCONFIGURE is used by gnome-common
+if test -z "$NOCONFIGURE"; then
+        if test -z "$*"; then
+                echo "I am going to run ./configure with no arguments - if you wish "
+                echo "to pass any to it, please specify them on the $0 command line."
+        fi
+fi
+
+rm -rf autom4te.cache
+
 gtkdocize || exit $?
-
-# we need to patch gtk-doc.make to support pretty output with
-# libtool 1.x.  Should be fixed in the next version of gtk-doc.
-# To be more resilient with the various versions of gtk-doc one
-# can find, just sed gkt-doc.make rather than patch it.
-sed -e 's#) --mode=compile#) --tag=CC --mode=compile#' gtk-doc.make > gtk-doc.temp \
-        && mv gtk-doc.temp gtk-doc.make
-sed -e 's#) --mode=link#) --tag=CC --mode=link#' gtk-doc.make > gtk-doc.temp \
-        && mv gtk-doc.temp gtk-doc.make
-
 ACLOCAL="${ACLOCAL-aclocal} $ACLOCAL_FLAGS" autoreconf -v --install || exit $?
+cd $ORIGDIR || exit $?
 
-./configure "$@" && echo Now type make to compile $PKG_NAME.
+if test -z "$NOCONFIGURE"; then
+        $srcdir/configure $AUTOGEN_CONFIGURE_ARGS "$@" || exit $?
+        echo "Now type 'make' to compile $PROJECT."
+fi
