e52250a7b81d7ea31107dfc413cf17dd75d88b24
Emmanuele Bassi <ebassi@linux.intel.com>
2011-04-05 11:57:23 +0100

    Fix C99-isms in example code

--- examples/gtk-clutter-window-test.c
+++ examples/gtk-clutter-window-test.c
@@ -93,14 +93,17 @@ on_toolbar_leave (ClutterActor *actor,
 int
 main (int argc, char **argv)
 {
+    GtkWidget *window, *iconview, *sw, *toolbar;
+    GtkListStore *store;
+    ClutterActor *stage, *actor;
+
     gtk_clutter_init (&argc, &argv);
 
-    GtkWidget *window = gtk_clutter_window_new ();
+    window = gtk_clutter_window_new ();
     g_signal_connect (window, "destroy", G_CALLBACK (gtk_main_quit), NULL);
     gtk_window_set_default_size (GTK_WINDOW (window), 400, 300);
 
-    GtkListStore *store = gtk_list_store_new (N_COLUMNS,
-		    G_TYPE_STRING, GDK_TYPE_PIXBUF);
+    store = gtk_list_store_new (N_COLUMNS, G_TYPE_STRING, GDK_TYPE_PIXBUF);
     add_liststore_rows (store,
                         "devhelp",
                         "empathy",
@@ -111,20 +114,19 @@ main (int argc, char **argv)
                         "totem",
                         NULL);
 
-    GtkWidget *iconview = gtk_icon_view_new_with_model (GTK_TREE_MODEL (store));
+    iconview = gtk_icon_view_new_with_model (GTK_TREE_MODEL (store));
     gtk_icon_view_set_text_column (GTK_ICON_VIEW (iconview), NAME_COLUMN);
     gtk_icon_view_set_pixbuf_column (GTK_ICON_VIEW (iconview), PIXBUF_COLUMN);
 
-    GtkWidget *sw = gtk_scrolled_window_new (NULL, NULL);
-
+    sw = gtk_scrolled_window_new (NULL, NULL);
     gtk_container_add (GTK_CONTAINER (window), sw);
     gtk_container_add (GTK_CONTAINER (sw), iconview);
     gtk_widget_show_all (sw);
 
     /* Widget 2 is a toolbar */
-    ClutterActor *stage = gtk_clutter_window_get_stage (GTK_CLUTTER_WINDOW (window));
+    stage = gtk_clutter_window_get_stage (GTK_CLUTTER_WINDOW (window));
 
-    GtkWidget *toolbar = gtk_toolbar_new ();
+    toolbar = gtk_toolbar_new ();
     add_toolbar_items (GTK_TOOLBAR (toolbar),
                        GTK_STOCK_ADD,
                        GTK_STOCK_BOLD,
@@ -135,7 +137,7 @@ main (int argc, char **argv)
                        NULL);
 
     gtk_widget_show_all (toolbar);
-    ClutterActor *actor = gtk_clutter_actor_new_with_contents (toolbar);
+    actor = gtk_clutter_actor_new_with_contents (toolbar);
     clutter_actor_add_constraint (actor, clutter_bind_constraint_new (stage, CLUTTER_BIND_WIDTH, 0.0));
     g_signal_connect (actor, "enter-event", G_CALLBACK (on_toolbar_enter), NULL);
     g_signal_connect (actor, "leave-event", G_CALLBACK (on_toolbar_leave), NULL);
