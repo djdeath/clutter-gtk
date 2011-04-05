5d45b2016c84cc87a7e862b0ac3a9be43b24dda7
Emmanuele Bassi <ebassi@linux.intel.com>
2011-04-05 11:57:23 +0100

    Fix C99-isms in example code

--- examples/gtk-clutter-window-test2.c
+++ examples/gtk-clutter-window-test2.c
@@ -28,91 +28,94 @@ animation_stage_1_complete (ClutterTimeline *timeline, ClutterActor *texture)
 {
     GtkAllocation allocation;
     GtkWidget *image;
+    ClutterAnimation *animation;
 
     image = GTK_WIDGET (g_object_get_data (G_OBJECT (texture), "image"));
     gtk_widget_get_allocation (image, &allocation);
 
     /* do the second animation, have the icon grow out from the middle of the
      * button */
-    ClutterAnimation *animation = clutter_actor_animate (texture,
-            CLUTTER_EASE_OUT_SINE,
-            100,
-            "fixed::x", (float) allocation.x,
-            "fixed::y", (float) allocation.y,
-            "fixed::scale-x", 0.,
-            "fixed::scale-y", 0.,
-            "scale-x", 1.,
-            "scale-y", 1.,
-            "fixed::scale-gravity", CLUTTER_GRAVITY_CENTER,
-            NULL);
+    animation = clutter_actor_animate (texture, CLUTTER_EASE_OUT_SINE, 100,
+                                       "fixed::x", (float) allocation.x,
+                                       "fixed::y", (float) allocation.y,
+                                       "fixed::scale-x", 0.,
+                                       "fixed::scale-y", 0.,
+                                       "scale-x", 1.,
+                                       "scale-y", 1.,
+                                       "fixed::scale-gravity", CLUTTER_GRAVITY_CENTER,
+                                       NULL);
 
     g_signal_connect_after (clutter_animation_get_timeline (animation),
-                "completed",
-                G_CALLBACK (animation_stage_2_complete),
-                texture);
+                            "completed",
+                            G_CALLBACK (animation_stage_2_complete),
+                            texture);
 }
 
 static void
 button_clicked (GtkButton *button, char *stock_id)
 {
+    GtkWidget *toplevel;
+
     g_print ("button clicked: %s\n", stock_id);
 
     /* this whole function is really, really hacky and should never, ever
      * be copied into a real program. It's just a demonstration of what
      * is hypothetically possible with GTK+ CSW and Clutter */
 
-    GtkWidget *toplevel = gtk_widget_get_toplevel (GTK_WIDGET (button));
+    toplevel = gtk_widget_get_toplevel (GTK_WIDGET (button));
     if (GTK_CLUTTER_IS_WINDOW (toplevel))
     {
-
+        ClutterActor *texture, *stage;
+        GtkWidget *image;
         char *icon_name;
         GtkAllocation allocation;
         GtkAllocation toplevel_allocation;
         GtkIconSize size;
+        GdkPixbuf *blank;
+        ClutterAnimation *animation;
 
         /* create a texture from the button image */
-        ClutterActor *texture = gtk_clutter_texture_new ();
-        GtkWidget *image = gtk_button_get_image (button);
+        texture = gtk_clutter_texture_new ();
+        image = gtk_button_get_image (button);
         gtk_image_get_stock (GTK_IMAGE (image), &icon_name, &size);
         gtk_clutter_texture_set_from_stock (GTK_CLUTTER_TEXTURE (texture),
                                             GTK_WIDGET (button),
                                             icon_name, size, NULL);
 
         /* position the texture on top of the existing icon */
-        ClutterActor *stage = gtk_clutter_window_get_stage (GTK_CLUTTER_WINDOW (toplevel));
+        stage = gtk_clutter_window_get_stage (GTK_CLUTTER_WINDOW (toplevel));
         clutter_container_add_actor (CLUTTER_CONTAINER (stage), texture);
 
         g_object_set_data (G_OBJECT (texture), "image", image);
         g_object_set_data_full (G_OBJECT (texture), "stock-id",
-                g_strdup (icon_name), (GDestroyNotify) g_free);
+                                g_strdup (icon_name),
+                                (GDestroyNotify) g_free);
         g_object_set_data (G_OBJECT (texture), "icon-size",
-                GINT_TO_POINTER (size));
+                           GINT_TO_POINTER (size));
 
         gtk_widget_get_allocation (image, &allocation);
 
         /* replace the icon itself */
-        GdkPixbuf *blank = gdk_pixbuf_new (GDK_COLORSPACE_RGB,
-                                           TRUE, 8,
-                                           allocation.width,
-                                           allocation.height);
+        blank = gdk_pixbuf_new (GDK_COLORSPACE_RGB,
+                                TRUE, 8,
+                                allocation.width,
+                                allocation.height);
         gdk_pixbuf_fill (blank, 0x00000000);
         gtk_image_set_from_pixbuf (GTK_IMAGE (image), blank);
         g_object_unref (blank);
 
         /* animate a fall due to gravity */
         gtk_widget_get_allocation (toplevel, &toplevel_allocation);
-        ClutterAnimation *animation = clutter_actor_animate (texture,
-                CLUTTER_EASE_IN_QUAD,
-                200,
-                "fixed::x", (float) allocation.x,
-                "fixed::y", (float) allocation.y,
-                "y", (float) toplevel_allocation.height,
-                NULL);
+        animation = clutter_actor_animate (texture, CLUTTER_EASE_IN_QUAD, 200,
+                                           "fixed::x", (float) allocation.x,
+                                           "fixed::y", (float) allocation.y,
+                                           "y", (float) toplevel_allocation.height,
+                                           NULL);
 
         g_signal_connect_after (clutter_animation_get_timeline (animation),
-                "completed",
-                G_CALLBACK (animation_stage_1_complete),
-                texture);
+                                "completed",
+                                G_CALLBACK (animation_stage_1_complete),
+                                texture);
     }
 }
 
@@ -121,10 +124,12 @@ add_button (GtkTable *table, char *stock_id, int row)
 {
     GtkWidget *button = gtk_button_new_from_stock (stock_id);
     gtk_table_attach_defaults (GTK_TABLE (table), button,
-            row, row + 1, row, row + 1);
+                               row, row + 1,
+                               row, row + 1);
 
     g_signal_connect (button, "clicked",
-            G_CALLBACK (button_clicked), stock_id);
+                      G_CALLBACK (button_clicked),
+                      stock_id);
 
     return button;
 }
@@ -132,10 +137,13 @@ add_button (GtkTable *table, char *stock_id, int row)
 int
 main (int argc, char **argv)
 {
+    GtkWidget *window, *table;
+    GtkSettings *settings;
+
     gtk_clutter_init (&argc, &argv);
 
-    GtkWidget *window = gtk_clutter_window_new ();
-    GtkWidget *table = gtk_table_new (6, 6, TRUE);
+    window = gtk_clutter_window_new ();
+    table = gtk_table_new (6, 6, TRUE);
 
     add_button (GTK_TABLE (table), GTK_STOCK_OK, 0);
     add_button (GTK_TABLE (table), GTK_STOCK_CANCEL, 1);
@@ -151,11 +159,12 @@ main (int argc, char **argv)
      * of rely on this to be TRUE to actually show the stock
      * icon falling off the button
      */
-    GtkSettings *settings = gtk_widget_get_settings (window);
+    settings = gtk_widget_get_settings (window);
     g_object_set (settings, "gtk-button-images", TRUE, NULL);
 
-    g_signal_connect_swapped (window, "destroy",
-            G_CALLBACK (gtk_main_quit), NULL);
+    g_signal_connect (window, "destroy",
+                      G_CALLBACK (gtk_main_quit),
+                      NULL);
 
     gtk_main ();
 
