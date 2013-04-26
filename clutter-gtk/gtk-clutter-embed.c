9b64df88b855cb501d97987ced99ba879f8824cd
Bastien Nocera <hadess@hadess.net>
2013-04-26 18:22:25 +0200

    embed: Receive touch and smooth scroll events
    Otherwise all the clicks from touchscreens will appear as synthetised
    events coming from the virtual core pointer instead of touch events
    coming from the touchscreen itself.
    
    https://bugzilla.gnome.org/show_bug.cgi?id=698969

--- clutter-gtk/gtk-clutter-embed.c
+++ clutter-gtk/gtk-clutter-embed.c
@@ -309,7 +309,9 @@ gtk_clutter_embed_realize (GtkWidget *widget)
                         | GDK_KEY_RELEASE_MASK
                         | GDK_POINTER_MOTION_MASK
                         | GDK_ENTER_NOTIFY_MASK
-                        | GDK_LEAVE_NOTIFY_MASK;
+                        | GDK_LEAVE_NOTIFY_MASK
+                        | GDK_TOUCH_MASK
+                        | GDK_SMOOTH_SCROLL_MASK;
 
   attributes_mask = GDK_WA_X | GDK_WA_Y | GDK_WA_VISUAL;
 
